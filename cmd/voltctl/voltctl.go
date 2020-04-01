a1e53faab95f8a9dafcf71104cd3df1100e7d767
Scott Baker <smbaker@gmail.com>
2020-04-01 11:13:34 -0700

    VOL-1886 watch interadapter bus
    Change-Id: I14cd3a9d0ca80348aab80326c28cc56380f88882

--- cmd/voltctl/voltctl.go
+++ cmd/voltctl/voltctl.go
@@ -68,6 +68,7 @@ func main() {
 	commands.RegisterComponentCommands(parser)
 	commands.RegisterLogLevelCommands(parser)
 	commands.RegisterEventCommands(parser)
+	commands.RegisterMessageCommands(parser)
 
 	_, err = parser.Parse()
 	if err != nil {
