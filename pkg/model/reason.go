42923bc0401731ba3ff218620370a203fcea115b
Maninder <maninder@ciena.com>
2021-01-21 22:34:20 +0530

    VOL-3773 Add command to extract device reason and display it as an output
    Change-Id: I104ea3be17e2cf5073286c4f34c2949b7f680029

---
+++ pkg/model/reason.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019-present Ciena Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package model
+
+import (
+	"github.com/opencord/voltha-protos/v4/go/voltha"
+)
+
+/*
+ * This is a partial list of OF match/action values. This list will be
+ * expanded as new fields are needed within VOLTHA
+ *
+ * Strings are used in the output structure so that on output the table
+ * can be "sparsely" populated with "empty" cells as opposed to 0 (zeros)
+ * all over the place.
+ */
+type Reason struct {
+	DeviceID string `json:"deviceId"`
+	UniID    string `json:"uniId"`
+	Reason   string `json:"reason"`
+}
+
+func (f *Reason) PopulateFromProto(reason *voltha.DeviceReason) {
+	f.DeviceID = reason.DeviceId
+	f.UniID = reason.UniId
+	f.Reason = reason.Reason
+}
