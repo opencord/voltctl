e39cd270fab35231ee2c9f1e722aaa7c2addd169
David K. Bainbridge <dbainbri@ciena.com>
2021-03-23 01:26:10 +0000

    VOL-3943 - mutiple stack configurations in voltctl
    - support multiple stack configurations in voltctl configuration file
    - added stack command line option to dynamically switch stacks
    - stack subcommand to support stack configuration manipulations
    
    Change-Id: Ib482e4b190881755d3084fd5de6950c6106b069b

---
+++ internal/pkg/apis/config/v2/config.go
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021-present Ciena Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package config
+
+import (
+	"time"
+)
+
+type GrpcConfigSpec struct {
+	Timeout            time.Duration `yaml:"timeout"`
+	MaxCallRecvMsgSize string        `yaml:"maxCallRecvMsgSize"`
+}
+
+type KvStoreConfigSpec struct {
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+type TlsConfigSpec struct {
+	UseTls bool   `yaml:"useTLS"`
+	CACert string `yaml:"caCert"`
+	Cert   string `yaml:"cert"`
+	Key    string `yaml:"key"`
+	Verify bool   `yaml:"verify"`
+}
+
+type StackConfigSpec struct {
+	Name          string            `yaml:"name"`
+	Server        string            `yaml:"server"`
+	Kafka         string            `yaml:"kafka"`
+	KvStore       string            `yaml:"kvstore"`
+	Tls           TlsConfigSpec     `yaml:"tls"`
+	Grpc          GrpcConfigSpec    `yaml:"grpc"`
+	KvStoreConfig KvStoreConfigSpec `yaml:"kvstoreconfig"`
+	K8sConfig     string            `yaml:"-"`
+}
+
+type GlobalConfigSpec struct {
+	ApiVersion   string             `yaml:"apiVersion"`
+	Stacks       []*StackConfigSpec `yaml:"stacks"`
+	CurrentStack string             `yaml:"currentStack"`
+}
