553a1397401f682e2ca4f48fc276c80e85928a50
Himani Chawla <hichawla@ciena.com>
2021-06-10 23:39:17 +0530

    VOL-4029 fetch extended pm counters
    Change-Id: Ic5e0624a47c4f35a0013045722a25693bcd8f008

--- pkg/model/stats.go
+++ pkg/model/stats.go
@@ -45,3 +45,34 @@ type OnuStats struct {
 	// reported timestamp in seconds since epoch
 	Timestamp *uint32 `json:"timestamp,omitempty"`
 }
+
+type OnuEthernetFrameExtendedPm struct {
+	UDropEvents               *uint64 `json:"upstream_drop_events,omitempty"`
+	UOctets                   *uint64 `json:"upstream_octets,omitempty"`
+	UFrames                   *uint64 `json:"upstream_frames,omitempty"`
+	UBroadcastFrames          *uint64 `json:"upstream_broadcast_frames,omitempty"`
+	UMulticastFrames          *uint64 `json:"upstream_multicast_frames,omitempty"`
+	UCrcErroredFrames         *uint64 `json:"upstream_crc_errored_frames,omitempty"`
+	UUndersizeFrames          *uint64 `json:"upstream_undersize_frames,omitempty"`
+	UOversizeFrames           *uint64 `json:"upstream_oversize_frames,omitempty"`
+	UFrames_64Octets          *uint64 `json:"upstream_frames_64_octets,omitempty"`
+	UFrames_65To_127Octets    *uint64 `json:"upstream_frames_65_to_127_octets,omitempty"`
+	UFrames_128To_255Octets   *uint64 `json:"upstream_frames_128_to_255_octets,omitempty"`
+	UFrames_256To_511Octets   *uint64 `json:"upstream_frames_256_to_511_octets,omitempty"`
+	UFrames_512To_1023Octets  *uint64 `json:"upstream_frames_512_to_1023_octets,omitempty"`
+	UFrames_1024To_1518Octets *uint64 `json:"upstream_frames_1024_to_1518_octets,omitempty"`
+	DDropEvents               *uint64 `json:"downstream_drop_events,omitempty"`
+	DOctets                   *uint64 `json:"downstream_octets,omitempty"`
+	DFrames                   *uint64 `json:"downstream_frames,omitempty"`
+	DBroadcastFrames          *uint64 `json:"downstream_broadcast_frames,omitempty"`
+	DMulticastFrames          *uint64 `json:"downstream_multicast_frames,omitempty"`
+	DCrcErroredFrames         *uint64 `json:"downstream_crc_errored_frames,omitempty"`
+	DUndersizeFrames          *uint64 `json:"downstream_undersize_frames,omitempty"`
+	DOversizeFrames           *uint64 `json:"downstream_oversize_frames,omitempty"`
+	DFrames_64Octets          *uint64 `json:"downstream_frames_64_octets,omitempty"`
+	DFrames_65To_127Octets    *uint64 `json:"downstream_frames_65_to_127_octets,omitempty"`
+	DFrames_128To_255Octets   *uint64 `json:"downstream_frames_128_to_255_octets,omitempty"`
+	DFrames_256To_511Octets   *uint64 `json:"downstream_frames_256_to_511_octets,omitempty"`
+	DFrames_512To_1023Octets  *uint64 `json:"downstream_frames_512_to_1023_octets,omitempty"`
+	DFrames_1024To_1518Octets *uint64 `json:"downstream_frames_1024_to_1518_octets,omitempty"`
+}
